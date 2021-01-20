469f3b440fd48f253ae4d4e24d9f2b10e337e8e0
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-01-20 17:13:04 +0330

    create packages

---
+++ Project-Phase3/src/AST/ASTNode.java
@@ -0,0 +1,68 @@
+package AST;
+
+
+import codegen.SymbolInfo;
+import codegen.SimpleVisitor;
+
+import java.util.List;
+
+/**
+ * The Abstract syn Node interface.
+ */
+public interface ASTNode {
+
+    /**
+     * Returns the node type.
+     */
+    NodeType getNodeType();
+
+    /**
+     * Sets the symbol info.
+     */
+    void setSymbolInfo(SymbolInfo si);
+
+    /**
+     * Gets the symbol info.
+     */
+    SymbolInfo getSymbolInfo();
+
+    /**
+     * Accepts a simple visitor.
+     */
+    void accept(SimpleVisitor visitor) throws Exception;
+
+    /**
+     * Adds a node to the end of the list of children.
+     */
+    void addChild(ASTNode node);
+
+    /**
+     * Adds a node to the list of children at the specified location.
+     */
+    void addChild(int index, ASTNode node);
+
+    /**
+     * Adds a list of nodes to the end of the list of children.
+     */
+    void addChildren(List<ASTNode> nodes);
+
+    /**
+     * Adds a list of nodes to the end of the list of children.
+     */
+    void addChild(ASTNode... nodes);
+
+    void setChildren(ASTNode... nodes);
+    /**
+     * Returns the list of children.
+     */
+    List<ASTNode> getChildren();
+
+    /**
+     * Returns the child at the specified location.
+     */
+    ASTNode getChild(int index);
+
+    void setParent(ASTNode parent);
+
+    ASTNode getParent();
+}
